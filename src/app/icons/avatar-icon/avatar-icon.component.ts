9b2d624e418d95bbe3649bcd3faf042375938c04
Sandra Zhen <szhen8930@gmail.com>
2021-04-24 01:48:32 -0400

    converted img to svg

---
+++ src/app/icons/avatar-icon/avatar-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'avatar-icon',
+  templateUrl: './avatar-icon.component.html',
+  styleUrls: ['./avatar-icon.component.scss']
+})
+export class AvatarIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import {DragIconComponent} from './drag-icon/drag-icon.component';
 import { DelIconComponent } from './del-icon/del-icon.component';
 import { EditIconComponent } from './edit-icon/edit-icon.component';
+import { AvatarIconComponent } from './avatar-icon/avatar-icon.component';
 
 
 @NgModule({
   declarations: [
     DragIconComponent,
     DelIconComponent,
-    EditIconComponent
+    EditIconComponent,
+    AvatarIconComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [DragIconComponent,DelIconComponent,EditIconComponent]
+  exports: [DragIconComponent,DelIconComponent
+    ,EditIconComponent,AvatarIconComponent]
 })
 export class IconsModule { }
